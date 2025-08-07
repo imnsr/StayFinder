ddce185997233aad2330ffdfc661c902ec0dac2a
KshitijBangde <kshitijbangde2110@gmail.com>
2025-08-07 18:24:26 +0530

    cors config removed from each services

--- backend/sf-propertymanagement/src/main/java/com/cdacproject/stayfinder/pg_property_service/config/SecurityConfig.java
+++ backend/sf-propertymanagement/src/main/java/com/cdacproject/stayfinder/pg_property_service/config/SecurityConfig.java
@@ -9,14 +9,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
